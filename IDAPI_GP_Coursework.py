e25f7abe8bc91426e9c50b6028ae39378f7eec6a
Wai Ong <who11@ic.ac.uk>
2018-02-18 13:42:43 +0000

    implemented msll

--- IDAPI_GP_Coursework.py
+++ IDAPI_GP_Coursework.py
@@ -300,7 +300,7 @@ class GaussianProcessRegression():
         # Task 7:
         # TODO: Implement MSLL of the prediction fbar, cov given the target ya
 
-        sigma2 = cov + self.k.sigma2_n
+        sigma2 = cov
         msll += 0.5*np.log(2*np.pi*sigma2)
         msll += np.square(ya-fbar)*0.5/sigma2
         msll = np.mean(msll)
